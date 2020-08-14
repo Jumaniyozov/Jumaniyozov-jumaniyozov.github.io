d4fca1817268b2ad40d98963c9fd331769349c7a
Jumaniyozov <JumaniyozovSE@gmail.com>
2020-08-14 16:14:43 +0500

    initial commit

---
+++ precache-manifest.8108349e7a2d3f4c6fe866caa09b4a0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6d92f61d76f6d9d0c1",
+    "url": "/css/app.ad411d90.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e64d3ae44f20efbf15ced7fa3b1bd79e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18278100bb5c838246e3",
+    "url": "/js/about.224d259a.js"
+  },
+  {
+    "revision": "5c6d92f61d76f6d9d0c1",
+    "url": "/js/app.32115159.js"
+  },
+  {
+    "revision": "8f8c282db62f517264d2",
+    "url": "/js/chunk-vendors.527ff5cd.js"
+  },
+  {
+    "revision": "e628dd11c948460d4af0088122385080",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
